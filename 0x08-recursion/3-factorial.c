e8c101c5339e0cd14716dcd182455019b87fe4d0
mauwaotuks <alfiemauwa7@gmail.com>
2022-11-12 15:02:32 -0800

    Task4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - calculate the factorial of a number
+ * @n: the number to calculate the factorial
+ *
+ * return: integer value
+ */
+
+int factorial(int n)
+{
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+
+}
