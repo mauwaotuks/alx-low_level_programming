82a48d6debfb96c44cc9e72d91ad0b1c9b357777
mauwaotuks <alfiemauwa7@gmail.com>
2022-11-14 17:53:33 -0800

    Task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - check if n is a prime number
+ * @n: int
+ * Return: 0 or 1
+ */
 
-/**
- * prime2 - Makes possible to evaluate from 1 to n
- * @a: same number as n
- * @b: number that iterates from 1 to n
- *
- * Return: on success 1.
- * on error, -1 is returned, and errno is set approprietely
- */
-int prime2(int a, int b)
-{
-	if (a == b)
-		return (1);
-	else if (a % b == 0)
-		return (0);
-	return (prime2(a, b = 1));
-}
-/**
- * is_prime_number - checks is a number is prime
- * @n: number integer
- *
- * Return: on success 1.
- * on error, -1 is returned, and errno is set approprietely
- */
+
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - check all number < n if they can divide it
+ * @n: int
+ * @resp: int
+ * Return: int
+ */
+
+int check_prime(int n, int resp)
+{
+
+	if (resp >= n && n > 1)
+		return (1);
+	else if (n % resp == 0 || n <= 1)
 		return (0);
-	return (prime2(n, 2));
+	else
+		return (check_prime(n, resp + 1));
 }
